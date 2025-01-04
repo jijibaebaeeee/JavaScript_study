3ba09e41e885179a0161286f3967520b407b5206
jijibaebaeeee <isnna1@naver.com>
2025-01-04 10:56:38 +0900

    이용약관 모두 동의 및 개별동의 이벤트설정

--- workspace/day28/myself_js/eventStudy6.js
+++ workspace/day28/myself_js/eventStudy6.js
@@ -6,4 +6,24 @@
 // terms의 checked 속성에 all의 checked 값을 대입한다
 
 // terms을 반복하며
-// 3개의 모든 체크박스가 체크되는 경우 all도 체크된다. 
+// 3개의 모든 체크박스가 체크되는 경우 all도 체크된다.
+NodeList.prototype.map = Array.prototype.map;
+
+const all = document.querySelector("input.all");
+// console.log(all);
+const terms = document.querySelectorAll("input.term");
+
+all.addEventListener('click', () =>{
+  terms.forEach((term) => {
+    term.checked = all.checked;
+  })
+})
+
+terms.forEach((term)=>{
+  term.addEventListener('click', () => {
+    console.log(term);
+    all.checked = terms.map(term => term.checked).filter((checked) => checked).length === 3;
+  });
+})
+
+//결론 : NodeList여도 map 함수를 사용하려면 NodeList의 prototype에 Array의 prototype의 map을 추가해줘야한다.
